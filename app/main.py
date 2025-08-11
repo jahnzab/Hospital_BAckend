233f78d444ef54923880c593ccdfece4b8c81282
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-11 08:13:21 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI
-from app.routes import chat_routes, patient_routes, doctor_routes
-from app.auth import auth_routes
+from .auth import auth_routes
+from .routes import chat_routes, patient_routes, doctor_routes
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="My Project API")
+app = FastAPI(title="SHMS Booking API (Postgres)")
+origins = [
+    "http://localhost:3000",  # local React dev
+    "https://hospital-frontend-lilac.vercel.app/",  # production React on Render
+]
 
-# ✅ Include routers from routes/
-app.include_router(chat_routes.router, prefix="/chat", tags=["Chat"])
-app.include_router(patient_routes.router, prefix="/patients", tags=["Patients"])
-app.include_router(doctor_routes.router, prefix="/doctors", tags=["Doctors"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # allow specific domains
+    allow_credentials=True,
+    allow_methods=["*"],          # allow all HTTP methods
+    allow_headers=["*"],          # allow all headers
+)
 
-# ✅ Include auth routes
-app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
+app.include_router(auth_routes.router)
+app.include_router(chat_routes.router)
+app.include_router(patient_routes.router)
+app.include_router(doctor_routes.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to My Project API"}
-
+    return {"status": "ok", "service": "SHMS Booking API"}
