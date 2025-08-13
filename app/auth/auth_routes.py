fce29bcc641a654f0b6203a805089ee7baf7e31e
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-13 10:47:50 +0530

    Update auth_routes.py

--- app/auth/auth_routes.py
+++ app/auth/auth_routes.py
@@ -17,6 +17,11 @@ def get_db():
 @router.post("/login", response_model=Token)
 def login(data: UserLogin, db: Session = Depends(get_db)):
     user = db.query(Users).filter(Users.username == data.username).first()
+    print("DEBUG: user from DB:", user)
+    if user:
+     print("DEBUG: stored hash:", user.password_hash)
+     print("DEBUG: incoming password:", data.password)
+
     if not user or not verify_password(data.password, user.password_hash):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
     token = create_access_token({"sub": user.username, "role": user.role, "doctor_id": user.doctor_id})
